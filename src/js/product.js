0548891d47dc0f87c59ba7fc5e6e9e1f58d95f71
Tiago <tiagorockman@gmail.com>
2025-05-08 23:17:11 -0300

    fix but to add at card

--- src/js/product.js
+++ src/js/product.js
@@ -4,6 +4,8 @@ import ProductData from "./ProductData.mjs";
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
+  const cartItems = getLocalStorage("so-cart") || []; // get cart array of items from local storage if null set to empty array
+  cartItems.push(product);
   setLocalStorage("so-cart", product);
 }
 // add to cart button event handler
