ab098924518e8fc05605e7c8c9e3aa79241b69be
Tiago Neves <tiagorockman@gmail.com>
2025-06-14 19:46:07 -0300

    doc rank and tuition information

--- docs/scripts/search.js
+++ docs/scripts/search.js
@@ -281,7 +281,7 @@ function createUniversityCard(university) {
         </div>
         <div class="text-right">
           <div class="text-sm text-gray-500">Ranking</div>
-          <div class="font-bold text-blue-600">#${university.ranking ?? "#0"}</div>
+          <div class="font-bold text-blue-600">#${university.URank ?? "0"}</div>
         </div>
       </div>
       
@@ -299,17 +299,17 @@ function createUniversityCard(university) {
             </svg>
             <span class="text-sm">Tuition</span>
           </div>
-          <span class="font-semibold text-gray-900">${university.tuition ?? "No information"}</span>
+          <span class="font-semibold text-gray-900">${university.Tuition_and_fees ?? "No information"}</span>
         </div>
         
         <div class="flex items-center justify-between gap-2">
           <div class="flex items-center text-gray-600 ">
-            <svg class="h-4 w-4 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+            <svg class="h-4 w-4 mr-1 .gap-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"></path>
             </svg>
             <span class="text-sm">Acceptance</span>
           </div>
-          <span class="font-semibold text-gray-900">${university.hi_offer}</span>
+          <span class="font-semibold text-gray-900 ">${university.hi_offer}%</span>
         </div>
         
         <div class="flex items-center justify-between">
@@ -321,7 +321,7 @@ function createUniversityCard(university) {
             </svg>
             <span class="text-sm">Programs</span>
           </div>
-          <span class="font-semibold text-gray-900">${university.size_set}+</span>
+          <span class="font-semibold text-gray-900">${0}+</span>
         </div>
       </div>
       
